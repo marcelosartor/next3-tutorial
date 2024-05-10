5b64b037221471cbb9f35573511eae9f650fb15b
marcelosartor <marcelo_sartor@hotmail.com>
2024-05-10 18:27:00 -0300

    aula 07

--- components/videos/Detalhes.vue
+++ components/videos/Detalhes.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Componentes Nuxt 3</h1>
     <p>Autor: Marcelo Sartor</p>
+    <p>{{diferencaEmDias}} Dias atrás</p>
   </div>
 </template>
+<script setup lang="ts">
+  const {diasPassados,diferencaEmDias} = useFormatacao()
+  const dataVideo = new Date(2023,9,2)
+  diasPassados(dataVideo)
 
+</script>
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,4 @@
-const logado =  false
+const logado =  true
 export default defineNuxtRouteMiddleware((to,from)=>{
   if( to.path !== '/login' && !logado ){
     return navigateTo("/login")
