f3b3e9dde4867fb3b9edc03e1fb1265aa1ea40f2
marcelosartor <marcelo_sartor@hotmail.com>
2024-05-12 21:51:58 -0300

    aula08

--- pages/videos/favoritos.vue
+++ pages/videos/favoritos.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-    Videos favoritos
+  <div> Videos favoritos </div>
+  <div class="videos">
+    <div v-for="video in favoritos" :key="video.id">
+      <h2>{{ video.descricao }}</h2>
+      <iframe width="550" height="400" :src="video.url" title="Youtube video player" frameborder="0">
+      </iframe>
+  
+      <div>
+        <button @click="removerFavoritos(video.id)">Remover Favorito</button>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
+const favoritos = useFavoritos()
+
+const removerFavoritos = (id:number) =>{
+  favoritos.value = favoritos.value.filter((video)=> video.id !== id)
+}
 
 </script>
 
--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,10 +1,75 @@
 <template>
-  <div>
-    Videos
+  <div>Videos</div>
+  
+  <NuxtLink to="/videos/favoritos">Favoritos</NuxtLink>
+  <div class="videos">
+    <div v-for="video in videos" :key="video.id">
+      <h2>{{ video.descricao }}</h2>
+      <p>{{ converteDataBrasil(video.data_postagem) }}</p>
+      <iframe width="550" height="400" :src="video.url" title="Youtube video player" frameborder="0">
+      </iframe>
+   
+      <div>
+        <button @click="adicionarFavoritos(video)">Adicionar Favorito</button>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
+import { Video } from '@/interfaces/video';
+
+  const favoritos = useFavoritos()
+    
+  const videos: Video[] = [
+    {
+      id: 1,
+      descricao: '01-Introduçao e Instalação',
+      url: 'https://www.youtube.com/embed/WahQ5AoXpuU?si=F2FrXqWjB1dtJvGw',
+      data_postagem: '2023-10-15'
+    },
+    {
+      id: 2,
+      descricao: '03-Configuração',
+      url: 'https://www.youtube.com/embed/JbbB84bnPog?si=CBRrE5OSXmxbZ2ZA',
+      data_postagem: '2023-10-20'
+    },
+    {
+      id: 3,
+      descricao: '03-Pages',
+      url: 'https://www.youtube.com/embed/S99sVicr8NI?si=awG5U6hDT-kHlEK4',
+      data_postagem: '2023-10-10'
+    },
+    {
+      id: 4,
+      descricao: '04-Components',
+      url: 'https://www.youtube.com/embed/cCHWanw49l8?si=b-a-31JJqjXxleoV',
+      data_postagem: '2023-10-05'
+    },
+  ]
+
+  const converteDataBrasil = (dataAtual:string) => {
+    return new Date(dataAtual).toLocaleDateString("pt-BR")
+  }
+
+  const adicionarFavoritos = (video: Video) => {
+    favoritos.value.push(video)
+  }
 
 </script>
 
+<style scoped>
+  .videos {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  .videos button {
+    display: inline-block;
+  }
+
+</style>
